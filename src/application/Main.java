09a25f883a99812fbe6295a1349376fda7c4de14
Ryan Pham <rqn.pham@gmail.com>
2023-10-07 08:48:47 -0700

    Commit containing overhaul, adding a MainController package, making it easier to create the interface with SceneBuilder

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+			package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+
+public class Main extends Application {
+	
+	/*
+	 * When the program is ran, the start function is initially ran.
+	 * 
+	 * @param primaryStage the JavaFX stage
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			HBox root = (HBox) FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getClassLoader().getResource("css/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/controller/MainController.java
@@ -0,0 +1,59 @@
+package application.controller;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class MainController {
+	
+	@FXML HBox mainBox;
+	@FXML HBox newProject;
+	
+	/*
+	 * If the back button is pressed, the function to show the homepage is run.
+	 */
+	@FXML public void showHomepage() {
+		URL url = getClass().getClassLoader().getResource("view/Main.fxml");
+		
+		try {
+			// Stage is fetched
+			Stage stage = (Stage) newProject.getScene().getWindow(); 
+			HBox pane1 = (HBox)FXMLLoader.load(url);
+			Scene scene = new Scene(pane1);
+			// Set scene
+			stage.setScene(scene);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	/*
+	 * If the back button is pressed, the function to show the new project page is run.
+	 */
+	@FXML public void showNewProjectPage() {
+		
+		URL url = getClass().getClassLoader().getResource("view/NewProject.fxml");
+		
+		try {
+			// Stage is fetched
+			Stage stage = (Stage) mainBox.getScene().getWindow(); 
+			HBox pane1 = (HBox)FXMLLoader.load(url);
+			Scene scene = new Scene(pane1);
+			// Set scene
+			stage.setScene(scene);
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
